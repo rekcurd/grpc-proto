1f4f36469d14c1d975a4a89e89df90ce2ebee93e
Keigo Hattori <keigoht@gmail.com>
2018-08-08 18:20:27 +0900

    Remove unnecessary code

--- run_codegen.sh
+++ run_codegen.sh
@@ -3,4 +3,4 @@ echo "[drucker-grpc-proto run_codegen.sh START]"
 
 cd `dirname $0`
 
-python -m grpc.tools.protoc -I/usr/local/include -I. --python_out=../ --grpc_python_out=../ drucker.proto
+python -m grpc.tools.protoc -I. --python_out=../ --grpc_python_out=../ drucker.proto

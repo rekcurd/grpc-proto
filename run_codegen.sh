5e57f2f4c30576e705acd61d851ce8a4045ce036
Hattori Keigo <keigoht@gmail.com>
2018-10-16 12:56:02 +0900

    Add compiled files

--- run_codegen.sh
+++ run_codegen.sh
@@ -3,4 +3,4 @@ echo "[drucker-grpc-proto run_codegen.sh START]"
 
 cd `dirname $0`
 
-python -m grpc.tools.protoc -I. --python_out=../ --grpc_python_out=../ drucker.proto
+python -m grpc.tools.protoc -I. --python_out=./protobuf --grpc_python_out=./protobuf drucker.proto

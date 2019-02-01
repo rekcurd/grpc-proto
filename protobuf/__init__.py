7c78b51c6d2b6b1f6836575855c34c6bc1220144
Hattori Keigo <keigoht@gmail.com>
2019-02-01 13:39:09 +0900

    Replace from `drucker` to `rekcurd`

--- protobuf/__init__.py
+++ protobuf/__init__.py
@@ -10,5 +10,5 @@ root_path = pathlib.Path(os.path.abspath(__file__)).parent
 sys.path.append(str(root_path))
 
 
-from .drucker_pb2 import *
-from .drucker_pb2_grpc import *
+from .rekcurd_pb2 import *
+from .rekcurd_pb2_grpc import *
--- run_codegen.sh
+++ run_codegen.sh
@@ -3,4 +3,4 @@ echo "[grpc-proto run_codegen.sh START]"
 
 cd `dirname $0`
 
-python -m grpc.tools.protoc -I. --python_out=./protobuf --grpc_python_out=./protobuf drucker.proto
+python -m grpc.tools.protoc -I. --python_out=./protobuf --grpc_python_out=./protobuf rekcurd.proto
